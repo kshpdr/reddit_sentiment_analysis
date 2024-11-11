9c388d9a04d5928e6858e8a296aa95448b39688c
srikamal <srikamal2001@gmail.com>
2024-11-11 16:27:41 -0500

    Updated BERT with larger context window

--- src/bert_sentiment_analysis.py
+++ src/bert_sentiment_analysis.py
@@ -14,7 +14,6 @@ def load_csv():
 			title = row[1]
 			self_text = row[2]
 			post_text = title + " " + self_text
-			post_text = post_text[:100]
 			entries.append(post_text)
 	return entries[1:]  # disregard the attribute names
 
@@ -23,13 +22,12 @@ def write_labels(generated_sentiment_labels):
 	f = open(write_file, "a")
 	for label in generated_sentiment_labels:
 		assigned_label = label['label']
-		score = label['score']
 		final_label = 0  # let default assumption be neutral
-		if assigned_label == 'NEU':
+		if assigned_label == 'LABEL_1':
 			final_label = 0
-		elif assigned_label == 'POS':
+		elif assigned_label == 'LABEL_2':
 			final_label = 1
-		elif assigned_label == 'NEG':
+		elif assigned_label == 'LABEL_0':
 			final_label = -1
 		f.write(str(final_label))
 		f.write("\n")
@@ -46,7 +44,8 @@ def main():
 	print(f"found {len(entries)} posts")
 
 	start = time.time()
-	sentiment_analysis_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+	# sentiment_analysis_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+	sentiment_analysis_model = pipeline(model="cardiffnlp/twitter-roberta-base-sentiment", truncation=True, max_length=512)   # Truncate inputs over the limit
 	end = time.time()
 	print(f"Loaded sentiment analysis pipeline in {end - start} seconds")
 	labels = sentiment_analysis_model(entries)
